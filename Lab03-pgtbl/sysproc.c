b9cb9879ffc2fb9ef5799c4fbf73dceadf2c3fec
yanxing <97666089+csyanxing@users.noreply.github.com>
2023-08-04 00:00:51 +0800

    add lab3 code

---
+++ Lab03-pgtbl/sysproc.c
@@ -0,0 +1,121 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  argint(0, &n);
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  uint64 page_addr;
+  int n;
+  uint64 out_addr;
+  argaddr(0, &page_addr);
+  argint(1, &n);
+  argaddr(2, &out_addr);
+  uint64 result = 0;
+
+  pagetable_t pagetable = myproc()->pagetable;
+  for (int i = 0; i < n; i++) {
+    uint64 va = page_addr + i * PGSIZE;
+    pte_t *pte0 = walk(pagetable, va, 0);
+    if(*pte0 & PTE_A){
+      // printf("i:%d\n", i);
+      *pte0 &= ~(PTE_A);
+      result |= (1 << i);
+    }
+  }
+  copyout(pagetable, out_addr, (char*)(&result), n / 8);
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
